42440c60834bd343fef4d959651e936a03207fb9
storyture <pablo@minerva.kgi.edu>
2021-02-28 12:23:21 -0800

    Changed ALLOWED_HOSTS in settings.py

--- smore/settings.py
+++ smore/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-ALLOWED_HOSTS = ['swipo.herokuapp.com']
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'swipo.herokuapp.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
